03cfe1f1f0c941108c15ae2d2a916b2d4c9deac5
Weihong Guan <weihong.guan@gmail.com>
2013-07-03 10:05:30 +0800

    try input capture

--- src/demo.cpp
+++ src/demo.cpp
@@ -22,7 +22,25 @@ void setup() {
 	TIM_OutputNState_Disable, 1);
 	t4_oc4.apply(TIM4, TIM_OC4Init);
 
+	TIM_ICInitTypeDef TIM_ICInitStructure;
+	TIM_ICInitStructure.TIM_Channel = TIM_Channel_1;
+	TIM_ICInitStructure.TIM_ICPolarity = TIM_ICPolarity_Rising;
+	TIM_ICInitStructure.TIM_ICSelection = TIM_ICSelection_DirectTI;
+	TIM_ICInitStructure.TIM_ICPrescaler = TIM_ICPSC_DIV1;
+	TIM_ICInitStructure.TIM_ICFilter = 0x0;
+	TIM_ICInit(TIM1, &TIM_ICInitStructure);
+
+	TIM_ICInitStructure.TIM_ICPolarity = TIM_ICPolarity_Falling;
+	TIM_ICInitStructure.TIM_ICSelection = TIM_ICSelection_IndirectTI;
+	TIM_ICInit(TIM1, &TIM_ICInitStructure);
+
+	TIM_SelectInputTrigger(TIM1, TIM_TS_TI1FP1);
+	TIM_SelectSlaveMode(TIM1, TIM_SlaveMode_Reset);
+	TIM_SelectMasterSlaveMode(TIM1, TIM_MasterSlaveMode_Enable);
+
 	Tim t1(TIM1, RCC_APB2Periph_TIM1, RCC_APB2PeriphClockCmd);
+//	t1.init(24000000);
+//	RCC_APB2PeriphClockCmd(RCC_APB2Periph_TIM1, ENABLE);
 
 	nvic.init(TIM2_IRQn, 0, 3, ENABLE);
 	Tim t2(TIM2, RCC_APB1Periph_TIM2, RCC_APB1PeriphClockCmd);
@@ -40,6 +58,12 @@ void loop() {
 	}
 
 	led_blue.toggle();
+
+	if (TIM_GetITStatus(TIM1, TIM_IT_CC1) == SET) {
+		TIM_ClearITPendingBit(TIM1, TIM_IT_CC1);
+		fprintf(stdout, "%d\r\n", TIM_GetCapture1(TIM1));
+	}
+
 	delay(1000);
 }
 
