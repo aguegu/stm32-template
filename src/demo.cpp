78f7c27fe9fc81a231dc41ea4a9c9d319888e0b1
Weihong Guan <weihong.guan@gmail.com>
2013-07-03 10:48:27 +0800

    use pwm to trig

--- src/demo.cpp
+++ src/demo.cpp
@@ -11,43 +11,25 @@ void setup() {
 	led_green.init(GPIO_Mode_Out_PP);
 	led_blue.init(GPIO_Mode_Out_PP);
 
-	ultrasonic_sensor_trig.init(GPIO_Mode_AF_PP, GPIO_Speed_50MHz);
-	ultrasonic_sensor_echo.init(GPIO_Mode_IN_FLOATING, GPIO_Speed_50MHz);
-
-	Tim t4(TIM4, RCC_APB1Periph_TIM4, RCC_APB1PeriphClockCmd);
-	t4.init(100000, 10000);
-
-	TimOc t4_oc4;
-	t4_oc4.init(TIM_OCMode_PWM1, TIM_OutputState_Enable,
-	TIM_OutputNState_Disable, 1);
-	t4_oc4.apply(TIM4, TIM_OC4Init);
-
-	TIM_ICInitTypeDef TIM_ICInitStructure;
-	TIM_ICInitStructure.TIM_Channel = TIM_Channel_1;
-	TIM_ICInitStructure.TIM_ICPolarity = TIM_ICPolarity_Rising;
-	TIM_ICInitStructure.TIM_ICSelection = TIM_ICSelection_DirectTI;
-	TIM_ICInitStructure.TIM_ICPrescaler = TIM_ICPSC_DIV1;
-	TIM_ICInitStructure.TIM_ICFilter = 0x0;
-	TIM_ICInit(TIM1, &TIM_ICInitStructure);
-
-	TIM_ICInitStructure.TIM_ICPolarity = TIM_ICPolarity_Falling;
-	TIM_ICInitStructure.TIM_ICSelection = TIM_ICSelection_IndirectTI;
-	TIM_ICInit(TIM1, &TIM_ICInitStructure);
-
-	TIM_SelectInputTrigger(TIM1, TIM_TS_TI1FP1);
-	TIM_SelectSlaveMode(TIM1, TIM_SlaveMode_Reset);
-	TIM_SelectMasterSlaveMode(TIM1, TIM_MasterSlaveMode_Enable);
-
-	Tim t1(TIM1, RCC_APB2Periph_TIM1, RCC_APB2PeriphClockCmd);
-//	t1.init(24000000);
-//	RCC_APB2PeriphClockCmd(RCC_APB2Periph_TIM1, ENABLE);
-
 	nvic.init(TIM2_IRQn, 0, 3, ENABLE);
 	Tim t2(TIM2, RCC_APB1Periph_TIM2, RCC_APB1PeriphClockCmd);
 	t2.init(1000, 1000);
 	TIM_ITConfig(TIM2, TIM_IT_Update, ENABLE);
 	TIM_Cmd(TIM2, ENABLE);
 
+	ultrasonic_sensor_trig.init(GPIO_Mode_AF_PP, GPIO_Speed_50MHz);
+	ultrasonic_sensor_echo.init(GPIO_Mode_IPD, GPIO_Speed_50MHz);
+
+	//ultrasonic_sensor_trig.set(Bit_RESET);
+
+	Tim t4(TIM4, RCC_APB1Periph_TIM4, RCC_APB1PeriphClockCmd);
+	t4.init(100000, 10000);
+
+	TimOc t4_oc4;
+	t4_oc4.init(TIM_OCMode_PWM1, TIM_OutputState_Enable,
+			TIM_OutputNState_Disable, 1);
+
+	t4_oc4.apply(TIM4, TIM_OC4Init);
 }
 
 void loop() {
@@ -59,10 +41,18 @@ void loop() {
 
 	led_blue.toggle();
 
-	if (TIM_GetITStatus(TIM1, TIM_IT_CC1) == SET) {
-		TIM_ClearITPendingBit(TIM1, TIM_IT_CC1);
-		fprintf(stdout, "%d\r\n", TIM_GetCapture1(TIM1));
+//	ultrasonic_sensor_trig.set(Bit_SET);
+//	ultrasonic_sensor_trig.set(Bit_RESET);
+
+	uint32_t t = 0;
+
+	while (ultrasonic_sensor_echo.getInput() == Bit_RESET)
+		;
+	while (ultrasonic_sensor_echo.getInput() == Bit_SET) {
+		t++;
 	}
+
+	fprintf(stdout, "%ld\r\n", t);
 
 	delay(1000);
 }
