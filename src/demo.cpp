abec324b932f68bd58d740d77cec94b300097129
Weihong Guan <weihong.guan@gmail.com>
2013-07-03 14:28:45 +0800

    servo sample updated as tim class upgrade

--- src/demo.cpp
+++ src/demo.cpp
@@ -1,8 +1,9 @@
 #include "stm32-template.h"
 
-Gpio led_green(GPIOC, GPIO_Pin_9, RCC_APB2Periph_GPIOC );
-Gpio led_blue(GPIOC, GPIO_Pin_8, RCC_APB2Periph_GPIOC );
+Gpio led_green(GPIOC, GPIO_Pin_9, RCC_APB2Periph_GPIOC);
+Gpio led_blue(GPIOC, GPIO_Pin_8, RCC_APB2Periph_GPIOC);
 Gpio servo(GPIOB, GPIO_Pin_9, RCC_APB2Periph_GPIOB);
+TimOc t4_oc4(TIM4, TIM_OC4Init, TIM_SetCompare4);
 
 void setup() {
 
@@ -13,11 +14,8 @@ void setup() {
 	Tim t4(TIM4, RCC_APB1Periph_TIM4, RCC_APB1PeriphClockCmd);
 	t4.init(1000000, 20000);
 
-	TimOc t4_oc4;
-	t4_oc4.init(TIM_OCMode_PWM1, TIM_OutputState_Enable, TIM_OutputNState_Disable, 1000);
-	t4_oc4.apply(TIM4, TIM_OC4Init);
-
-	TIM_Cmd(TIM4, ENABLE);
+	t4_oc4.init(TIM_OCMode_PWM1, TIM_OutputState_Enable,
+			TIM_OutputNState_Disable);
 
 	nvic.init(TIM2_IRQn, 0, 3, ENABLE);
 	Tim t2(TIM2, RCC_APB1Periph_TIM2, RCC_APB1PeriphClockCmd);
@@ -31,6 +29,7 @@ void loop() {
 	while (usart.available()) {
 		char c = usart.read();
 		fprintf(stdout, "0x%02X\r\n", c);
+		t4_oc4.setCompare((uint16_t) c * 100);
 		led_blue.toggle();
 	}
 
