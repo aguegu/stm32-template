ed83ac9c62af6216369193d2f3b1db6c4a758953
Weihong Guan <weihong.guan@gmail.com>
2013-08-16 00:48:11 +0800

    init st7920 demo

--- src/demo.cpp
+++ src/demo.cpp
@@ -1,7 +1,17 @@
 #include "stm32-template.h"
+#include "st7920/st7920.h"
 
 Gpio led_green(GPIOC, GPIO_Pin_9, RCC_APB2Periph_GPIOC );
 Gpio led_blue(GPIOC, GPIO_Pin_8, RCC_APB2Periph_GPIOC );
+
+Gpio st7920_rs(GPIOB, GPIO_Pin_9, RCC_APB2Periph_GPIOB);
+Gpio st7920_en(GPIOB, GPIO_Pin_8, RCC_APB2Periph_GPIOB);
+Gpio st7920_d4(GPIOB, GPIO_Pin_7, RCC_APB2Periph_GPIOB);
+Gpio st7920_d5(GPIOB, GPIO_Pin_6, RCC_APB2Periph_GPIOB);
+Gpio st7920_d6(GPIOB, GPIO_Pin_5, RCC_APB2Periph_GPIOB);
+Gpio st7920_d7(GPIOB, GPIO_Pin_4, RCC_APB2Periph_GPIOB);
+
+St7920 lcd(st7920_rs, st7920_en, st7920_d4, st7920_d5, st7920_d6, st7920_d7);
 
 void setup() {
 
@@ -13,6 +23,11 @@ void setup() {
 	t2.init(1000, 1000);
 	t2.configureIT(TIM_IT_Update);
 	t2.setState();
+
+	lcd.init();
+
+	//lcd.clear();
+	lcd.display();
 }
 
 void loop() {
