3aa1c7fc8046b1f037e19a088682aeef0de26cd8
Weihong Guan <weihong.guan@gmail.com>
2013-08-16 15:25:58 +0800

    create a display function in demo

--- src/demo.cpp
+++ src/demo.cpp
@@ -15,6 +15,10 @@ Gpio st7920_d7(GPIOB, GPIO_Pin_4, RCC_APB2Periph_GPIOB);
 
 St7920 lcd(st7920_rs, st7920_en, st7920_d4, st7920_d5, st7920_d6, st7920_d7);
 
+DotMatrix dm(128, 64);
+
+void display(uint8_t *p);
+
 void setup() {
 
 	led_green.init(GPIO_Mode_Out_PP);
@@ -28,36 +32,9 @@ void setup() {
 
 	lcd.init();
 
-	//lcd.clear();
-//	uint8_t *p = (uint8_t *)malloc(sizeof(uint8_t) * 1024);
-//	memset(p, 0x0f, 512);
-//	memset(p + 512, 0xf0, 512);
-//
-
-
-	DotMatrix dm(128, 64);
-	dm.setLine(0,0, 127, 63);
-
-	uint8_t *ppa = dm.output();
-	uint8_t *ppb = dm.output() + 512;
-
-	for (uint8_t r = 0; r < 0x20; r++) {
-		lcd.setDdRam(r); // y
-		lcd.setDdRam(0x00); // x
-
-		lcd.transmit(true, ppa, 0x10);
-		lcd.transmit(true, ppb, 0x10);
-
-		ppa += 0x10;
-		ppb += 0x10;
-	}
-//
-//	for (uint8_t r = 0; r < 0x20; r++) {
-//		lcd.setDdRam(r); // y
-//		lcd.setDdRam(0x08); // x
-//		for (uint8_t i = 16; i--;)
-//			lcd.writeData(*pp++);
-//	}
+	dm.setLine(0, 0, 127, 63);
+	dm.setLine(0, 31, 127, 31);
+	dm.setMoveDirection(DotMatrix::BIT_IN_COL_NEGA);
 }
 
 void loop() {
@@ -71,7 +48,22 @@ void loop() {
 
 	fprintf(stdout, "0x%02x\r\n", i++);
 
-	//led_blue.toggle();
-	delayMicroseconds(500000);
+	display(dm.output());
+	dm.move(true);
 }
 
+void display(uint8_t *p) {
+	uint8_t *ppa = p;
+	uint8_t *ppb = p + 512;
+
+	for (uint8_t r = 0; r < 0x20; r++) {
+		lcd.setDdRam(r); // y
+		lcd.setDdRam(0x00); // x
+
+		lcd.transmit(true, ppa, 0x10);
+		lcd.transmit(true, ppb, 0x10);
+
+		ppa += 0x10;
+		ppb += 0x10;
+	}
+}
