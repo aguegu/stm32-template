65a38e99c6fd141ab30f151da67b10afa000e9fc
Weihong Guan <weihong.guan@gmail.com>
2013-08-16 13:22:35 +0800

    test st7920 mapping

--- src/demo.cpp
+++ src/demo.cpp
@@ -1,8 +1,9 @@
 #include "stm32-template.h"
 #include "st7920/st7920.h"
+#include <cstring>
 
-Gpio led_green(GPIOC, GPIO_Pin_9, RCC_APB2Periph_GPIOC );
-Gpio led_blue(GPIOC, GPIO_Pin_8, RCC_APB2Periph_GPIOC );
+Gpio led_green(GPIOC, GPIO_Pin_9, RCC_APB2Periph_GPIOC);
+Gpio led_blue(GPIOC, GPIO_Pin_8, RCC_APB2Periph_GPIOC);
 
 Gpio st7920_rs(GPIOB, GPIO_Pin_9, RCC_APB2Periph_GPIOB);
 Gpio st7920_en(GPIOB, GPIO_Pin_8, RCC_APB2Periph_GPIOB);
@@ -27,7 +28,32 @@ void setup() {
 	lcd.init();
 
 	//lcd.clear();
-	lcd.display();
+	uint8_t *p = (uint8_t *)malloc(sizeof(uint8_t) * 1024);
+	memset(p, 0x0f, 512);
+	memset(p + 512, 0xf0, 512);
+
+	uint8_t *ppa = p;
+	uint8_t *ppb = p + 512;
+
+	for (uint8_t r = 0; r < 0x20; r++) {
+		lcd.setDdRam(r); // y
+		lcd.setDdRam(0x00); // x
+
+		lcd.transmit(true, ppa, 0x10);
+		lcd.transmit(true, ppb, 0x10);
+
+		ppa += 0x10;
+		ppb += 0x10;
+	}
+//
+//	for (uint8_t r = 0; r < 0x20; r++) {
+//		lcd.setDdRam(r); // y
+//		lcd.setDdRam(0x08); // x
+//		for (uint8_t i = 16; i--;)
+//			lcd.writeData(*pp++);
+//	}
+
+	free(p);
 }
 
 void loop() {
