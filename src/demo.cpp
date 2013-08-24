35e298ea88da3d219c0a58e3c87eeb0ce01b94ac
Weihong Guan <weihong.guan@gmail.com>
2013-08-24 20:18:54 +0800

    change i2c_demo from ds3231 to at12c08 eeprom chip

--- src/demo.cpp
+++ src/demo.cpp
@@ -1,11 +1,9 @@
 #include "stm32-template.h"
-#include "ds3231/ds3231.h"
 
 Gpio led_green(GPIOC, GPIO_Pin_9, RCC_APB2Periph_GPIOC);
 Gpio led_blue(GPIOC, GPIO_Pin_8, RCC_APB2Periph_GPIOC);
 
 I2c i2c(I2C1, RCC_APB1Periph_I2C1);
-Ds3231 ds(i2c);
 
 Gpio i2c_scl(GPIOB, GPIO_Pin_6, RCC_APB2Periph_GPIOB);
 Gpio i2c_sda(GPIOB, GPIO_Pin_7, RCC_APB2Periph_GPIOB);
@@ -24,10 +22,7 @@ void setup() {
 	i2c_scl.init(GPIO_Mode_AF_OD);
 	i2c_sda.init(GPIO_Mode_AF_OD);
 
-	ds.init();
-
-	ds.setTime(12, 34, 56);
-	ds.setDate(13, 8, 21, 3);
+	i2c.init();
 }
 
 void loop() {
@@ -37,18 +32,16 @@ void loop() {
 		led_blue.toggle();
 	}
 
-	ds.refresh();
+	uint8_t c[2] = { 0x00, 0x00 };
+	i2c.write(0x50, c, 2, DISABLE);
+	i2c.read(0x50, c, 2, DISABLE);
+	fprintf(stdout, "0x%02X, 0x%02X\r\n", c[0], c[1]);
 
-	fprintf(stdout, "%02x-%02x-%02x %02x %02x:%02x:%02x %.2f\r\n",
-			ds.getHexData(YEAR),
-			ds.getHexData(MONTH),
-			ds.getHexData(DATE),
-			ds.getHexData(WEEKDAY),
-			ds.getHexData(HOUR),
-			ds.getHexData(MINUTE),
-			ds.getHexData(SECOND),
-			ds.getTemperature()
-	);
+	static uint8_t s[4] = { 0x00, 0x00, 0xbe, 0xef };
+	i2c.write(0x50, s, 4);
+
+	s[2]++;
+	s[3]++;
 
 	led_blue.toggle();
 	delay(1000);
