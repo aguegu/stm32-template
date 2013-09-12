8d41222a412770a4a4d1d6157db050ea00faffb0
Weihong Guan <weihong.guan@gmail.com>
2013-09-12 15:03:14 +0800

    ignore PB2, because it is BOOT1

--- src/demo.cpp
+++ src/demo.cpp
@@ -5,10 +5,37 @@ Gpio led_green(GPIOC, GPIO_Pin_9, RCC_APB2Periph_GPIOC);
 Gpio led_blue(GPIOC, GPIO_Pin_8, RCC_APB2Periph_GPIOC);
 GpioArray leds(2);
 
+Gpio dip_0(GPIOB, GPIO_Pin_14, RCC_APB2Periph_GPIOB);
+Gpio dip_1(GPIOB, GPIO_Pin_13, RCC_APB2Periph_GPIOB);
+Gpio dip_2(GPIOB, GPIO_Pin_12, RCC_APB2Periph_GPIOB);
+Gpio dip_3(GPIOB, GPIO_Pin_11, RCC_APB2Periph_GPIOB);
+Gpio dip_4(GPIOB, GPIO_Pin_10, RCC_APB2Periph_GPIOB);
+Gpio dip_5(GPIOB, GPIO_Pin_0, RCC_APB2Periph_GPIOB);
+Gpio dip_6(GPIOB, GPIO_Pin_1, RCC_APB2Periph_GPIOB);
+
+// Gpio dip_7(GPIOB, GPIO_Pin_2, RCC_APB2Periph_GPIOB);
+// PB2 is BOOT1, which is pull down by external resistor
+// just leave it alone
+
+
+GpioArray dips(7);
+
 void setup() {
 	leds.setGpio(0, led_blue);
 	leds.setGpio(1, led_green);
 	leds.init(GPIO_Mode_Out_PP);
+
+	leds.set(0x01);
+
+	dips.setGpio(0, dip_0);
+	dips.setGpio(1, dip_1);
+	dips.setGpio(2, dip_2);
+	dips.setGpio(3, dip_3);
+	dips.setGpio(4, dip_4);
+	dips.setGpio(5, dip_5);
+	dips.setGpio(6, dip_6);
+
+	dips.init(GPIO_Mode_IPU);
 
 	nvic.configure(TIM2_IRQn, 0, 3, ENABLE);
 	Tim t2(TIM2, RCC_APB1Periph_TIM2, RCC_APB1PeriphClockCmd);
@@ -19,15 +46,14 @@ void setup() {
 
 void loop() {
 
-	static uint8_t i = 0;
 	while (usart.available()) {
 		char c = usart.read();
 		fprintf(stdout, "0x%02X\r\n", c);
 	}
 
-	fprintf(stdout, "0x%02x\r\n", i++);
+	fprintf(stdout, "0x%02lx\r\n", dips.getInput());
 
-	leds.set(i & 0x01 ? 0x01 : 0x02);
+	leds.toggle();
 
 	delayMicroseconds(500000);
 }
