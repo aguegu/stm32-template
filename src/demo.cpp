0c526a047bdfd7b7854785417f55d76247de1952
Weihong Guan <weihong.guan@gmail.com>
2013-09-12 10:52:14 +0800

    Gpio objects should be declared as global to keep its lifetime

--- src/demo.cpp
+++ src/demo.cpp
@@ -1,16 +1,13 @@
 #include "stm32-template.h"
 #include "gpio-array/gpio-array.h"
 
+Gpio led_green(GPIOC, GPIO_Pin_9, RCC_APB2Periph_GPIOC);
+Gpio led_blue(GPIOC, GPIO_Pin_8, RCC_APB2Periph_GPIOC);
 GpioArray leds(2);
 
 void setup() {
-
-	Gpio led_green(GPIOC, GPIO_Pin_9, RCC_APB2Periph_GPIOC);
-	Gpio led_blue(GPIOC, GPIO_Pin_8, RCC_APB2Periph_GPIOC);
-
 	leds.setGpio(0, led_blue);
 	leds.setGpio(1, led_green);
-
 	leds.init(GPIO_Mode_Out_PP);
 
 	nvic.configure(TIM2_IRQn, 0, 3, ENABLE);
