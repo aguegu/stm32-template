1500ef81843e9c2b9822e8646fb121b238f7dbaf
Weihong Guan <weihong.guan@gmail.com>
2013-10-17 03:18:06 +0800

    iv22 with ultrasonic

--- src/demo.cpp
+++ src/demo.cpp
@@ -1,10 +1,19 @@
 #include "stm32-template.h"
 
+#include "tubes/iv22.h"
+
 Gpio led_green(GPIOC, GPIO_Pin_9, RCC_APB2Periph_GPIOC);
 Gpio led_blue(GPIOC, GPIO_Pin_8, RCC_APB2Periph_GPIOC);
 
-Gpio ultrasonic_sensor_trig(GPIOB, GPIO_Pin_9, RCC_APB2Periph_GPIOB);
+Gpio ultrasonic_sensor_trig(GPIOB, GPIO_Pin_8, RCC_APB2Periph_GPIOB);
 Gpio ultrasonic_sensor_echo(GPIOA, GPIO_Pin_8, RCC_APB2Periph_GPIOA);
+
+Gpio tube_din(GPIOB, GPIO_Pin_6, RCC_APB2Periph_GPIOB);
+Gpio tube_oe(GPIOB, GPIO_Pin_5, RCC_APB2Periph_GPIOB);
+Gpio tube_st(GPIOB, GPIO_Pin_4, RCC_APB2Periph_GPIOB);
+Gpio tube_sh(GPIOB, GPIO_Pin_3, RCC_APB2Periph_GPIOB);
+
+Iv22 tubes(tube_din, tube_oe, tube_st, tube_sh, 4);
 
 void setup() {
 
@@ -26,11 +35,11 @@ void setup() {
 	Tim t4(TIM4, RCC_APB1Periph_TIM4, RCC_APB1PeriphClockCmd);
 	t4.init(100000, 10000);
 
-	TimOc t4_oc4(TIM4, TIM_OC4Init, TIM_SetCompare4);
-	t4_oc4.init(TIM_OCMode_PWM1,
-			TIM_OutputState_Enable,
-			TIM_OutputNState_Disable);
-	t4_oc4.setCompare(1);
+	TimOc t4_oc3(TIM4, TIM_OC3Init, TIM_SetCompare3);
+	t4_oc3.init(TIM_OCMode_PWM1,
+	TIM_OutputState_Enable,
+	TIM_OutputNState_Disable);
+	t4_oc3.setCompare(1);
 
 	Tim t1(TIM1, RCC_APB2Periph_TIM1, RCC_APB2PeriphClockCmd);
 	t1.init(2000000);
@@ -47,6 +56,8 @@ void setup() {
 	TIM_SelectMasterSlaveMode(TIM1, TIM_MasterSlaveMode_Enable);
 
 	TIM_ITConfig(TIM1, TIM_IT_CC2, ENABLE);
+
+	tubes.init();
 }
 
 void loop() {
@@ -60,7 +71,15 @@ void loop() {
 
 	led_blue.toggle();
 
-	fprintf(stdout, "%d cm %d\r\n", capture / 58 / 2, capture);
+	uint16_t mm = 5 * capture / 58;
+	fprintf(stdout, "%d mm %d\r\n", mm, capture);
+
+	tubes.clear();
+	tubes.setBackgroundColor((Color) (mm / 500 % 8));
+	tubes.printf("%4d", mm);
+	tubes.setPoint(2);
+	tubes.display();
+
 	delay(200);
 
 }
