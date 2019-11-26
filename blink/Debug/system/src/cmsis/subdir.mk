e3e202e7e08097e1ac5a276dad2528dde8e2736c
Weihong Guan <weihong.guan@gmail.com>
2019-11-26 20:29:19 +0800

    universal cflags

--- blink/Debug/system/src/cmsis/subdir.mk
+++ blink/Debug/system/src/cmsis/subdir.mk
@@ -1,27 +1,21 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-# Add inputs and outputs from these tool invocations to the build variables 
+# Add inputs and outputs from these tool invocations to the build variables
 C_SRCS += \
 ../system/src/cmsis/system_stm32f10x.c \
-../system/src/cmsis/vectors_stm32f10x.c 
+../system/src/cmsis/vectors_stm32f10x.c
 
 OBJS += \
 ./system/src/cmsis/system_stm32f10x.o \
-./system/src/cmsis/vectors_stm32f10x.o 
+./system/src/cmsis/vectors_stm32f10x.o
 
 C_DEPS += \
 ./system/src/cmsis/system_stm32f10x.d \
-./system/src/cmsis/vectors_stm32f10x.d 
+./system/src/cmsis/vectors_stm32f10x.d
 
 
 # Each subdirectory must supply rules for building sources it contributes
 system/src/cmsis/%.o: ../system/src/cmsis/%.c
 	@echo 'Building file: $<'
 	@echo 'Invoking: GNU ARM Cross C Compiler'
-	arm-none-eabi-gcc -mcpu=cortex-m3 -mthumb -Og -fmessage-length=0 -fsigned-char -ffunction-sections -fdata-sections -ffreestanding -flto -fno-move-loop-invariants -Werror -Wunused -Wuninitialized -Wall -Wextra -Wmissing-declarations -Wconversion -Wpointer-arith -Wpadded -Wshadow -Wlogical-op -Waggregate-return -Wfloat-equal  -g3 -DDEBUG -DUSE_FULL_ASSERT -DSTM32F10X_MD -DUSE_STDPERIPH_DRIVER -DHSE_VALUE=8000000 -I"../include" -I"../system/include" -I"../system/include/cmsis" -I"${STD_PERIPH_INC}" -std=gnu11 -Wmissing-prototypes -Wstrict-prototypes -Wbad-function-cast -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -c -o "$@" "$<"
+	arm-none-eabi-gcc $(CXXFLAGS) -I"../include" -I"../system/include" -I"../system/include/cmsis" -I"${STD_PERIPH_INC}" $(GCCFLAGS) -MF"$(@:%.o=%.d)" -MT"$(@)" -c -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
-
-
