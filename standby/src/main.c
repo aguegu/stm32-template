052cf9490e7d20a16099d449dfe6e99eb93f061c
Weihong Guan <weihong.guan@gmail.com>
2019-12-17 10:59:37 +0800

    PA0 do wakeup and exti0

---
+++ standby/src/main.c
@@ -0,0 +1,94 @@
+#include "cmsis_device.h"
+#include "cortexm/ExceptionHandlers.h"
+
+void delay(u32);
+void EXTI0_IRQHandler(void);
+
+vu32 millis = 0;
+
+void delay(u32 ms) {
+	uint32_t start = millis;
+
+	while (ms) {
+		if (millis - start) {
+			ms--;
+			start++;
+		}
+    __WFI();
+	}
+}
+
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wunused-parameter"
+
+uint16_t t = 500;
+
+void main(int argc, char* argv[]) {
+
+  SysTick_Config(SystemCoreClock / 1000);
+  RCC_APB1PeriphClockCmd(RCC_APB1Periph_PWR,ENABLE);
+	// SysTick_CLKSourceConfig(SysTick_CLKSource_HCLK_Div8);
+  RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOC, ENABLE);
+
+  PWR_WakeUpPinCmd(ENABLE);
+
+  GPIO_InitTypeDef GPIO_InitStructure;
+  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_13;
+  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_OD;
+  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_2MHz;
+  GPIO_Init(GPIOC, &GPIO_InitStructure);
+
+  EXTI_InitTypeDef EXTI_InitStructure;
+  NVIC_InitTypeDef NVIC_InitStructure;
+
+  RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);
+
+  /* Configure PA.00 pin as input floating */
+  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0;
+  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN_FLOATING;
+  GPIO_Init(GPIOA, &GPIO_InitStructure);
+
+  /* Enable AFIO clock */
+  RCC_APB2PeriphClockCmd(RCC_APB2Periph_AFIO, ENABLE);
+
+  /* Connect EXTI0 Line to PA.00 pin */
+  GPIO_EXTILineConfig(GPIO_PortSourceGPIOA, GPIO_PinSource0);
+
+  /* Configure EXTI0 line */
+  EXTI_InitStructure.EXTI_Line = EXTI_Line0;
+  EXTI_InitStructure.EXTI_Mode = EXTI_Mode_Interrupt;
+  EXTI_InitStructure.EXTI_Trigger = EXTI_Trigger_Rising;
+  EXTI_InitStructure.EXTI_LineCmd = ENABLE;
+  EXTI_Init(&EXTI_InitStructure);
+
+  /* Enable and set EXTI0 Interrupt to the lowest priority */
+  NVIC_InitStructure.NVIC_IRQChannel = EXTI0_IRQn;
+  NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0x0F;
+  NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0x0F;
+  NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
+  NVIC_Init(&NVIC_InitStructure);
+
+  while (1) {
+    GPIO_WriteBit(GPIOC, GPIO_Pin_13, (BitAction)(!GPIO_ReadOutputDataBit(GPIOC, GPIO_Pin_13)));
+    delay(t);
+  }
+}
+
+#pragma GCC diagnostic pop
+
+void SysTick_Handler (void) {
+  millis++;
+}
+
+void EXTI0_IRQHandler(void)
+{
+  if(EXTI_GetITStatus(EXTI_Line0) != RESET) {
+    EXTI_ClearITPendingBit(EXTI_Line0);
+    if (t == 500) {
+      t = 100;
+    } else if (t == 100) {
+      t = 500;
+      PWR_EnterSTANDBYMode();
+    }
+  }
+}
